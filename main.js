1bc6b4d9a6796b14a8cda24495272478d29b8a9d
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2025-01-12 22:11:27 -0600

    view items function refactored for more dynamic use. Currently having styling issues w/ items view and single merchant view of items

--- main.js
+++ main.js
@@ -9,26 +9,49 @@ const merchantsNavButton = document.querySelector("#merchants-nav")
 const itemsNavButton = document.querySelector("#items-nav")
 const addNewButton = document.querySelector("#add-new-button")
 const showingText = document.querySelector("#showing-text")
+const singleMerchantView = document.querySelector("#single-merchant-view")
+const viewMerchantItemsButton = document.querySelector(".view-merchant-items")
+const addNewItemButton = document.querySelector("#add-new-item-button")
 
 //Form elements
 const merchantForm = document.querySelector("#new-merchant-form")
 const newMerchantName = document.querySelector("#new-merchant-name")
 const submitMerchantButton = document.querySelector("#submit-merchant")
+const itemForm = document.querySelector("#new-item-form")
+const newItemName = document.querySelector("#new-item-name")
+const newItemDescription = document.querySelector("#new-item-description")
+const newItemPrice = document.querySelector("#new-item-price")
+const merchantId = document.querySelector("#merchant-id")
+const submitItemButton = document.querySelector("#submit-item")
 
 // Event Listeners
 merchantsView.addEventListener('click', (event) => {
   handleMerchantClicks(event)
 })
+itemsView.addEventListener('click', (event) => {
+  handleItemClicks(event)
+})
 
 merchantsNavButton.addEventListener('click', showMerchantsView)
 itemsNavButton.addEventListener('click', showItemsView)
+
+viewMerchantItemsButton.addEventListener('click', showMerchantItemsView)
+
 
 addNewButton.addEventListener('click', () => {
   show([merchantForm])
 })
 
+addNewItemButton.addEventListener('click', () => {
+  show([itemForm])
+  })
+
 submitMerchantButton.addEventListener('click', (event) => {
   submitMerchant(event)
+})
+
+submitItemButton.addEventListener('click', (event) => {
+  submitItem(event)
 })
 
 //Global variables
@@ -61,6 +84,12 @@ function handleMerchantClicks(event) {
   }
 }
 
+function handleItemClicks(event) {
+  if (event.target.classList.contains("delete-item")) {
+    deleteItem(event)
+  }
+}
+
 function deleteMerchant(event) {
   const id = event.target.closest("article").id.split('-')[1]
   deleteData(`merchants/${id}`)
@@ -70,6 +99,18 @@ function deleteMerchant(event) {
       merchants.splice(indexOfMerchant, 1)
       displayMerchants(merchants)
       showStatus('Success! Merchant removed!', true)
+    })
+}
+
+function deleteItem(event) {
+  const id = event.target.closest("article").id.split('-')[1]
+  deleteData(`items/${id}`)
+    .then(() => {
+      let deletedItem = findItem(id)
+      let indexOfItem = items.indexOf(deletedItem)
+      items.splice(indexOfItem, 1)
+      displayItems(items)
+      showStatus('Success! Item removed!', true)
     })
 }
 
@@ -123,13 +164,45 @@ function submitMerchant(event) {
     })
 }
 
+function submitItem(event) {
+  event.preventDefault();
+
+  const itemName = newItemName.value;
+  const itemDesc = newItemDescription.value;
+  const itemPrice = newItemPrice.value; 
+  const merchId = merchantId.value; 
+  
+  if (!itemName || !itemDesc || !itemPrice || !merchId) {
+    showStatus('Please fill in all fields.', false);
+    return;
+  }
+
+  postData('items', { name: itemName, description: itemDesc, unit_price: itemPrice, merchant_id: parseInt(merchId) })
+    .then(postedItem => {
+      items.push(postedItem.data);
+      displayAddedItem(postedItem.data);
+  
+      newItemName.value = '';
+      newItemDescription.value = '';
+      newItemPrice.value = ''
+      merchantId.value = ''
+    
+      showStatus('Success! Item added!', true);
+      hide([itemForm]);
+    })
+    .catch(error => {
+      showStatus('Error: Item could not be added.', false);
+      console.error(error);
+    });
+}
+
 // Functions that control the view 
 function showMerchantsView() {
   showingText.innerText = "All Merchants"
   addRemoveActiveNav(merchantsNavButton, itemsNavButton)
   addNewButton.dataset.state = 'merchant'
   show([merchantsView, addNewButton])
-  hide([itemsView])
+  hide([itemsView, addNewItemButton])
   displayMerchants(merchants)
 }
 
@@ -139,26 +212,32 @@ function showItemsView() {
   addNewButton.dataset.state = 'item'
   show([itemsView])
   hide([merchantsView, merchantForm, addNewButton])
-  displayItems(items)
+  displayItems(items, itemsView)
 }
 
 function showMerchantItemsView(id, items) {
+  console.log('items:', items)
   showingText.innerText = `All Items for Merchant #${id}`
-  show([itemsView])
-  hide([merchantsView, addNewButton])
   addRemoveActiveNav(itemsNavButton, merchantsNavButton)
   addNewButton.dataset.state = 'item'
-  displayItems(items)
+  show([singleMerchantView, addNewItemButton])
+  hide([merchantsView, itemsView, addNewButton])
+  displayItems(items, singleMerchantView)
 }
 
 // Functions that add data to the DOM
-function displayItems(items) {
-  itemsView.innerHTML = ''
+function displayItems(items, view) {
+  if (itemsView === view || singleMerchantView === view){ 
+  view.innerHTML = ''}
+  if (items.length === 0) {
+    view.innerHTML = '<p>No Items Yet For This Merchant.</p>';
+    return; 
+  }
   let firstHundredItems = items.slice(0, 99)
   firstHundredItems.forEach(item => {
     let merchant = findMerchant(item.attributes.merchant_id).attributes.name
-    itemsView.innerHTML += `
-     <article class="item" id="item-${item.id}">
+    view.innerHTML += `
+    <article class="item" id="item-${item.id}">
           <img src="" alt="">
           <h2>${item.attributes.name}</h2>
           <p>${item.attributes.description}</p>
@@ -167,6 +246,7 @@ function displayItems(items) {
         </article>
     `
   })
+  
 }
 
 function displayMerchants(merchants) {
@@ -216,6 +296,22 @@ function displayMerchantItems(event) {
   showMerchantItemsView(merchantId, filteredMerchantItems)
 }
 
+function displayAddedItem(item) {
+  itemsView.insertAdjacentHTML('beforeend',
+  ` <article class="item" id="item-${item.id}">
+          <img src="" alt="">
+          <h2>${item.attributes.name}</h2>
+          <p>${item.attributes.description}</p>
+          <p>$${item.attributes.unit_price}</p>
+          <p>${findMerchant(item.attributes.merchant_id).attributes.name}</p>
+          <div>
+            <button class="delete-item icon">🗑️</button>
+          </div>
+        </article>`)   
+  }
+
+
+
 //Helper Functions
 function show(elements) {
   elements.forEach(element => {
@@ -235,15 +331,16 @@ function addRemoveActiveNav(nav1, nav2) {
 }
 
 function filterByMerchant(merchantId) {
-  const specificMerchantItems = []
+  // const specificMerchantItems = []
 
   const filtered = items.filter((item)=> {
-    return item.merchant_id === merchantId
+    return item.attributes.merchant_id === parseInt(merchantId)
   })
-  filtered.forEach((item) =>{
-    specificMerchantItems.push(item)
-  })
-  return specificMerchantItems
+  return filtered
+  // filtered.forEach((item) =>{
+  //   specificMerchantItems.push(item)
+  // })
+  // return specificMerchantItems
 }
 
 function findMerchant(id) {
